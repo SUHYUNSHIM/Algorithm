d27e818aa737279143c711e16567eeff13be27f1
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-04 01:15:16 +0900

    interpreter 3.8, queue

---
+++ Data Structure/Queue1.py
@@ -0,0 +1,21 @@
+import queue
+data_queue = queue.Queue()
+data_queue.put("hello_future")
+data_queue.put(127)
+
+print(data_queue.get())
+print(data_queue.qsize())
+
+print(data_queue.get())
+print(data_queue.qsize())
+
+##우선순위를 부여한 Queue도 만들 수 있다.
+data_queue = queue.PriorityQueue()
+data_queue.put((10, "korea")) ##데이터가 튜플로 들어가기 때문에 괄호를 두개 쓴다.
+data_queue.put((5, 1)) ##우선순위, 들어가는 데이터
+data_queue.put((15, "canada"))
+
+print(data_queue.qsize())
+print(data_queue.get())
+print(data_queue.get())
+
