a424c481baff7f5277f567cf773bd822a5f71681
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-06 15:46:43 +0900

    BFS,DFS 기초 코드

---
+++ WeekTitle/BFS and DFS/Graph2.py
@@ -0,0 +1,28 @@
+##DFS 알고리즘
+graph = dict()
+
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+
+def dfs(graph, start_node):
+    visited, need_visit = list(), list()
+    need_visit.append(start_node)
+
+    while need_visit:
+        node = need_visit.pop()
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+
+    return visited
+##needed_visit 스택, visited 큐.
+##BFS 자료구조는 두 개의 큐를 활용하는 데 반해, DFS는 스택과 큐를 활용한다.
