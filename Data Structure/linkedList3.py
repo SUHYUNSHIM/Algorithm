5ab91cdf57da37f97f7a9e2b2324fb07724696cd
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-15 14:22:17 +0900

    double linkedlist2

--- Data Structure/linkedList3.py
+++ Data Structure/linkedList3.py
@@ -24,6 +24,47 @@ class NodeMgmt:
                 print(node.data)
                 node = node.next
 
+        def search_from_head(self,data):
+            if self.head == None:
+                return False
+            node = self.head
+            while node:
+                if node.data == data:
+                    return node
+                else:
+                    node = node.next
+            return False
+
+        def search_from_tail(self, data):
+            if self.head == None:
+                return False
+
+            node = self.tail
+            while node:
+                if node.data == data:
+                    return node
+                else:
+                    node = node.prev
+            return False
+
+        def insert_before(self, data, before_data):
+            if self.head == None:
+                self.head = Node(data)
+                return True
+            else:
+                node = self.tail
+                while node.data != before_data:
+                    node = node.prev
+                    if node == None:
+                        return False
+                new = Node(data)
+                before_new = node.prev
+                before_new.next = new
+                new.prev = before_new
+                new.next = node
+                node.prev = new
+                return True
+
 double_linked_list = NodeMgmt(0)
 for data in range(1,10):
     double_linked_list.insert(data)
