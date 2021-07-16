90580d10e8edbcb7b62d6588fb5fdd27ba58750f
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-16 15:44:21 +0900

    coding basic

--- Data Structure/Stack2.py
+++ Data Structure/Stack2.py
@@ -1,6 +1,13 @@
-data_stack = list()
-data_stack.append(1)
+data_stack = list() ##=[]
+data_stack.append(5)
 data_stack.append(2)
-
+data_stack.append(3)
+data_stack.append(1)
+data_stack.pop()
+data_stack.append(1)
+data_stack.append(4)
 data_stack ## [1,2]
 data_stack.pop() ## 2
+
+print(data_stack)
+print(data_stack[::-1]) #최상단 원소부터 출력
