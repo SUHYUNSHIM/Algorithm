62ffd3dc0c27f42ce4b2a82646aaeca5552d0650
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-21 18:14:46 +0900

    solution3

--- WeekTitle/BFS and DFS/탐색 알고리즘 기초/BFS.py
+++ WeekTitle/BFS and DFS/탐색 알고리즘 기초/BFS.py
@@ -3,13 +3,14 @@ from collections import deque
 visited = [False]*9
 def bfs(graph, start, visit):
     queue = deque([start])
+    print(queue)
     #현재 노드를 방문 처리
     visited[start] = True
     #큐가 빌 때까지 반복
     while queue:
         #큐에서 하나의 원소를 뽑아 출력
         v =queue.popleft()
-        print(v,end='')
+        print(v,end=' ')
         #해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
         for i in graph[v]:
             if not visited[i]:
