b1da63c2fa5c1c3969cc3a49c05b58594db6d831
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-30 13:29:17 +0900

    최대공약수 유클리드 호제법 풀이, 봄버맨

--- Daily/day1/BJ2609.py
+++ Daily/day1/BJ2609.py
@@ -18,3 +18,41 @@ maxCommon = a * (b/minCommon)
 print (minCommon)
 print (int(maxCommon))
 
+#유클리드 호제법 + 0730
+'''
+2개의 자연수를 받아 최대공약수를 받기 위해 2부터
+두 자연수 중 작은 자연수까지 모두 나누어보면서 가장 큰 공약수를 구할 수 있다.
+
+정의
+2개의 자연수 a,b에 대하여 a가 b보다 클 때, a를 b로 나눈 나머지를 r이라 하면, a와 b의 
+최대 공약수는 b와 r의 최대공약수와 같다. 이 성질을 따라 b를 r로 나눈 나머지 r2를 구하고,
+다시 r을 r2로 나눈 나머지를 구하는 과정을 재귀를 타고 들어가다 나머지가 0이 되면 그 나누는 수가 a와 b의 
+최대공약수이다. 
+
+GCD(A,B) = GCD(B, A%B)
+if A % B == 0 -> GCD = B
+else GCD(B,A%B)
+
+최소 공배수는 -> 최대 공약수를 이용해서 푼다.
+'''
+'''
+a,b = map(int, input().split())
+def gcd(a,b):
+     if a%b == 0:
+        return b
+    else:
+        return gcd(b,a%b)
+
+if a>b:
+    gcd = gcd(a,b)
+    lcm = (a*b)
+    print(gcd)
+    print(lcm)
+else:
+    gcd = gcd(b,a)
+    lcm = (a*b)
+    print(gcd)
+    print(lcm)
+
+'''
+
---
+++ WeekTitle/BJ_solution_4/BJ16918.py
@@ -0,0 +1,53 @@
+import sys
+from collections import deque
+read = sys.stdin.readline
+
+dx = [1, -1, 0, 0]  # 동서남북 인접한 곳의 좌표를 구하기 위함
+dy = [0, 0, 1, -1]
+
+R, C, N = map(int, read().split()) #가로 칸의 수, 세로 칸의 수, 시간
+bomb = deque()
+N=N-1
+
+#리스트 입력
+board = [list(read()) for _ in range(R)]
+
+#함수
+#폭탄 탐지
+def findBomb():
+    for i in range(R):
+        for j in range(C):
+            if board[i][j] =='O':
+                bomb.append((i,j))
+#모두 설치
+def addBomb():
+    for i in range(R):
+        for j in range(C):
+            if board[i][j] != 'O':
+                board[i][j] ='O'
+#폭발
+def bombbomb():
+    while bomb:
+        x, y = bomb.popleft()
+        board[x][y] = '.'
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <=nx <R and 0 <=ny<C:
+                if board[nx][ny] == 'O':
+                    board[x][y] ='.'
+
+while N:
+    findBomb()
+    addBomb()
+    N = N-1
+    if N ==0:
+        break
+    bombbomb()
+    N = N-1
+for i in range(R):
+    for j in range(C):
+        print(board[i][j], end='')
+    print()
+
+
