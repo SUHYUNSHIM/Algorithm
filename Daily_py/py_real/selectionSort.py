7f55c0bedfbf0b02e4f5603a46184d0f90c58dac
suhyunshim <lisa9963suhyun@gmail.com>
2021-10-04 16:05:17 +0900

    selectionsort . test data

--- Daily_py/py_real/selectionSort.py
+++ Daily_py/py_real/selectionSort.py
@@ -1,7 +1,7 @@
 def selection_sort(data):
     for stand in range(len(data)-1):
-        lowest = stand
-        for index in range(stand+1, len(data)):
+        lowest = stand #기준이 되는 값
+        for index in range(stand+1, len(data)): #range의 경우 (a,b)일때 b-1까지의 범위를 갖기 때문에 len(data)로 했을 때 리스트의 인덱스와 일치한다.
             if data[lowest] > data[index]:
                 lowest = index
         data[lowest],data[stand] = data[stand],data[lowest]
