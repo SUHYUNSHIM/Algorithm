dac05a7d5e60b7f9e8ceaf4e584475ca1968131e
suhyunshim <lisa9963suhyun@gmail.com>
2021-10-08 12:49:14 +0900

    재귀 예제2

--- Daily_py/py_real/factorials.py
+++ Daily_py/py_real/factorials.py
@@ -36,3 +36,35 @@ def palindrome(string):
         return False
 
 print(palindrome("level"))
+
+##factorial2
+#정수 n에 대해 n이 홀수면 3x n+1 을 하고
+#n이 짝수면 n을 2로 나눈다.
+#이렇게 계속 진행해서 n이 결국 1이 될 때까지 2,3의 과정을 반복합니다.
+#예시 3 -> 3 10 5 16 8 4 2 1 이 출력.
+#해당 함수를 작성하시오.
+print("출력 시작")
+def nums(n):
+    if n == 1:
+        return n
+    else:
+        if n%2 == 1:
+            print(3*n+1)
+            return(nums(3*n+1))
+        elif n%2 == 0:
+            print(int(n/2))
+            return(nums(int(n/2)))
+print(nums(3))
+print("좀 더 깔끔하게, return 상태로 n을 저장한 뒤 처음 출력하는 아래의 방식이 맞다.")
+
+def nums(n):
+    print(n)
+    if n == 1:
+        return n
+    else:
+        if n%2 == 1:
+            return(nums(3*n+1))
+        elif n%2 == 0:
+            return(nums(int(n/2)))
+print(nums(3))
+
