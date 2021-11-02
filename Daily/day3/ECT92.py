c854978d67feafdfc061c1362e4e36f0a453e9d2
suhyunshim <lisa9963suhyun@gmail.com>
2021-11-02 22:28:46 +0900

    큰 수의 법칙- 다른 풀이

--- Daily/day3/ECT92.py
+++ Daily/day3/ECT92.py
@@ -21,6 +21,33 @@ while cnt > 0:
             cnt = 0
 print(sum)
 
+'''
+result = 0
+first = numbers[0]
+second = numbers[1]
+while True:
+    for i in range(K):
+        if M == 0:
+            break
+        result += first
+        M -=1 #반복문을 도는 동안 최댓값이 더해지고, 합을 완성할 수 있는 count의 수를 감소시킨다. 
+    if M == 0:
+        break
+    result += second
+    M -=1
+print(result)
+'''
+
+'''
+count = int((m/k+1)) * K
+count += m %(k+1)
+
+result = 0
+result += (count) * first
+result += (m-count) * second
+
+'''
+
 
 
 
