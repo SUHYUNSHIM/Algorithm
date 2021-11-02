43ebb1045efe7af54926096343e3a523e1f9b692
suhyunshim <lisa9963suhyun@gmail.com>
2021-11-02 17:30:44 +0900

    걸그룹 이름 맞추기

---
+++ Daily/day3/BJ16165.py
@@ -0,0 +1,37 @@
+N, M = map(int,input().split())
+member_table = [[] for i in range(N)] #그룹 수만큼 빈 리스트 생성
+quiz = list()
+group= {}
+
+for i in range(N):
+    group_name = input()
+    member_num = int(input())
+    for j in range(member_num):
+        member_name = input()
+        member_table[i].append(member_name)
+    group[group_name] = member_table[i]
+
+'''
+for i in range(N):
+    print(member_table[i])
+'''
+#print(group['twice'])
+'''
+for i in range(M):
+    quiz.append(input())
+    int(input())
+'''
+for i in range(M):
+    question = input()
+    kind = int(input())
+
+    if kind == 0: #멤버 이름
+        quiz.extend(sorted(group[question]))
+    elif kind == 1: #속한 그룹의 이름
+        for g, members in group.items():
+            if question in members:
+                quiz.append(g)
+                #print(g)
+
+for answer in quiz:
+    print(answer)
