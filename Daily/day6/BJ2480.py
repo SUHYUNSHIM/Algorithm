afd695be33e083ee95c332503e06005dbbb29b16
suhyunshim <lisa9963suhyun@gmail.com>
2021-12-26 00:19:27 +0900

    맞는 코드

--- Daily/day6/BJ2480.py
+++ Daily/day6/BJ2480.py
@@ -4,6 +4,7 @@ a, b, c = map(int,input().split())
 def rewards(d1,d2,d3):
     reward = 0
     dlist = []
+    clist = []
     dlist.append(d1)
     dlist.append(d2)
     dlist.append(d3)
@@ -12,11 +13,12 @@ def rewards(d1,d2,d3):
         return reward
     else:
         for i in dlist:
-            cnt = dlist.count(i)
-            if cnt == 2:
-                reward = 1000 + i * 100
+            clist.append(dlist.count(i))
+        for j in range(len(clist)):
+            if clist[j] == 2:
+                reward = 1000 + dlist[j] * 100
                 return reward
-            elif cnt == 1:
+            elif max(clist) == 1:
                 #print(dlist)
                 reward = max(dlist) * 100
                 return reward
