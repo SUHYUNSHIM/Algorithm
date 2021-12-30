fdc058e5ab832f1d8be716a0979b7bdfe00a8d09
suhyunshim <lisa9963suhyun@gmail.com>
2021-12-31 01:57:20 +0900

    단어 뒤집기 하는중2

--- Daily/day7/BJ17413.py
+++ Daily/day7/BJ17413.py
@@ -1,8 +1,33 @@
 word = input().split()
-reverse_name = ''
+
+temp_name = ''
+cnt = 0
+
+#aprint(word)
 for c in word:
+    reverse_name = ''
     for d in range(len(c)):
-        reverse_name = c[d] + reverse_name #빈칸 단위, <> 단위로 끊어서 뒤집기를 해야. 그 순서를 유지하도록
-print(reverse_name)
+        a = True
+        if c[d] == '<':
+
+            temp_name = temp_name + reverse_name
+            temp_name = temp_name + c[d]
+            d = d + 1
+            while a:
+                temp_name = temp_name + c[d]
+                d = d + 1
+                if c[d] == '>':
+                    temp_name = temp_name + c[d]
+                    a = False
+                    cnt = cnt + 1
+
+        reverse_name = c[d] + reverse_name + ''  # 빈칸 단위, <> 단위로 끊어서 뒤집기를 해야. 그 순서를 유지하도록
+    if cnt == 0:
+        temp_name = temp_name + reverse_name + ' '
+
+print(temp_name)
+#baekjoon online judge
+#<open>tag<close>
+
 
 
