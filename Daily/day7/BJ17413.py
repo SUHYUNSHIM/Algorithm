001e64349ad65dcc6a6eb3a742d18be8390be2ca
suhyunshim <lisa9963suhyun@gmail.com>
2021-12-30 23:57:01 +0900

    단어 뒤집기 하는중

--- Daily/day7/BJ17413.py
+++ Daily/day7/BJ17413.py
@@ -1,7 +1,8 @@
-word = input()
+word = input().split()
 reverse_name = ''
 for c in word:
-    reverse_name = c + reverse_name #빈칸 단위, <> 단위로 끊어서 뒤집기를 해야. 그 순서를 유지하도록
+    for d in range(len(c)):
+        reverse_name = c[d] + reverse_name #빈칸 단위, <> 단위로 끊어서 뒤집기를 해야. 그 순서를 유지하도록
 print(reverse_name)
 
 
